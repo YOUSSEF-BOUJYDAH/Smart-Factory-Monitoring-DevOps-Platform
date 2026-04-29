e110e538e4fdd69296bed47ce2b96e87eca0cf2f
Youssef Boujydah <boujydahyoussef2000@gmail.com>
2026-04-29 03:40:30 +0200

    add Prometheus and Grafana observability stack

--- app/api/api.py
+++ app/api/api.py
@@ -66,7 +66,7 @@ def metrics():
 
 
 if __name__ == "__main__":
-    host = os.getenv("API_HOST", "0.0.0.0")
-    port = int(os.getenv("API_PORT", "5000"))
+    host = os.getenv("APP_HOST", "0.0.0.0")
+    port = int(os.getenv("APP_PORT", "5000"))
 
     app.run(host=host, port=port)
